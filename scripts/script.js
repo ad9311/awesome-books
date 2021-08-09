364e7beb5467c5f466bf9541d2460794212bcc4f
Ángel Díaz <diazangel9311@gmail.com>
2021-08-09 16:17:30 -0500

    Add book constructor

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
-function test() {
+let books = [];
+
+function Book(author, title) {
+  this.author = author;
+  this.title = title;
+}
+
+function addBooks() {
   let author = document.getElementById('author');
   let title = document.getElementById('title');
   let li = document.createElement('li');
 
-  let id = `${author.value}-${title.value}`.toLowerCase();
+  let id = `${author.value}_${title.value}`.toLowerCase();
 
   li.id = id;
-  li.innerHTML = `<p>${author.value} - ${title.value}</p>`;
+  li.innerHTML = `<p>${author.value} - ${title.value}</p><input type="button" value="Remove" onclick="removeBooks(${id})">`;
 
+  books.push(new Book(author.value, title.value));
   document.getElementById('books').appendChild(li); 
+}
+
+function removeBooks(id) {
+  document.getElementById().remove();
 }
