52bcc7ed64ead4f9f2f8221626379ba1d3b08933
Chike1990 <chibuikeuzoechina@gmail.com>
2021-08-11 21:46:23 +0100

    Adds some padding and color

--- scripts/main.js
+++ scripts/main.js
@@ -43,6 +43,7 @@ function renderBooks() {
     const { id } = book;
     const li = document.createElement('li');
     li.id = id;
+    li.className = 'd-flex justify-content-between py-1';
     li.innerHTML = `<p>"${title}" by ${author} </p><input type="button" value="Remove" onclick="removeBooks(${id})"></hr>`;
     document.getElementById('books').appendChild(li);
   });
@@ -56,6 +57,7 @@ function addBooks() {
   const time = (currentDate.getSeconds() + currentDate.getMilliseconds()) * 2;
   const id = `book${time}`;
   li.id = id;
+  li.className = 'd-flex justify-content-between py-1';
   li.innerHTML = `<p>"${title.value}" by ${author.value} </p><input type="button" value="Remove" onclick="removeBooks(${id})"></hr>`;
   const book = new Book(author.value, title.value, id);
   bookCollection.saveBook(book);
